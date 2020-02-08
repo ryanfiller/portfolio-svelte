701c0c6eced340f61d2f6ec817272d727690d907
Ryan Filler <ryanfiller89@gmail.com>
2020-02-08 10:59:18 -0600

    refactor change component  - move component out misc directory  - refactor  - add animation library and accesiblity helper function  - add arrow css mixin

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = (props) => (
   <header
     style={{
-      background: 'rebeccapurple',
+      background: 'var(--primaryColor)',
       marginBottom: '1.45rem'
     }}
   >
