10068f19cfe0f272777ca9b59e420a8a94926e91
Ryan Filler <ryanfiller89@gmail.com>
2020-04-12 20:18:39 -0500

    add table helper function, move directory structure, add table markdown component and styles, fix console errors

--- static/fonts/font-importer.js
+++ static/fonts/font-importer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 import './fonts.css'
 
 const FontImporter = () => {
   return (
-    <>
+    <Helmet>
       <link
         rel='preload'
         as='font'
@@ -25,7 +26,7 @@ const FontImporter = () => {
         href={'./LabDJR-VF.woff'}
         type='font/woff'
       />
-    </>
+    </Helmet>
   )
 }
 
