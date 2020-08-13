58a494e1a91a5c6ca6cc287c92d4c904cf7b580b
Ryan Filler <ryanfiller89@gmail.com>
2020-08-13 17:55:38 -0500

    add "building a componenet in three frameworks" post

--- src/components/misc/color-chart.js
+++ src/components/misc/color-chart.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { default as Chart } from 'react-color-contrast-table'
+import Chart from 'color-contrast-table-react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
@@ -55,78 +55,28 @@ const ColorChart = props => {
     }))
   )
 
-  const handleInputChange = index => event => {
+  const handleValuesChanges = (index, newValue) => {
     const newColors = [...colors]
-    const { name, value } = event.target
-    newColors[index] = {name: name, value: value}
+    newColors[index].value = newValue
     setColors(newColors)
+    const { name, value } = newColors[index]
     setColor(name, value)
   }
   
   return (
     <section className='color-chart'>
-      {editable &&
-        <div className='color-chart__inputs'>
-          {colors.map((color, index) => (
-            <EditColor 
-              key={index}
-              index={index}
-              {...color}
-              onChange={handleInputChange}
-            />
-          ))}
-        </div>
-      }
-      <div 
-        className={cx(
-          'color-chart__table',
-          editable && 'color-chart__table--editable'
-        )}
-      >
         <Chart 
-          defaultStyles
+          useStyles
+          editValues={editable}
+          onValuesChange={handleValuesChanges}
           colors={colors}
         />
-      </div>
     </section>
-  )
-}
-
-const EditColor = props => {
-  const {
-    name,
-    value,
-    index,
-    onChange
-  } = props
-  
-  return(
-    <div className='color-chart__input'>
-      <label htmlFor={name} className='color-chart__input-label'>
-        <span className='color-chart__input-label--name'>{name.replace('color', '').toLowerCase()}</span>
-        <span className='color-chart__input-label--value'>{value}</span>
-      </label>
-      <input
-        type='color'
-        id={name}
-        name={name}
-        value={value}
-        onChange={onChange(index)}
-        disabled={noJs}
-      />
-    </div>
   )
 }
 
 ColorChart.propTypes = {
   editable: PropTypes.bool,
-}
-
-EditColor.propTypes = {
-  name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired,
-  onChange: PropTypes.func.isRequired
 }
 
 export default ColorChart
