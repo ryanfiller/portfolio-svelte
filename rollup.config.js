f46608448b1586a6573143505ef3328d5669fc87
Ryan Filler <ryanfiller89@gmail.com>
2021-03-04 16:24:31 -0600

    remove babel and babel config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import commonjs from '@rollup/plugin-commonjs'
 import svelte from 'rollup-plugin-svelte'
-import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import config from 'sapper/config/rollup.js'
 import pkg from './package.json'
@@ -28,7 +27,6 @@ import twitter from './plugins/rehype/twitter'
 
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
-const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
 // https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables
 let netlifyUrl = ''
@@ -113,24 +111,6 @@ export default {
 					{ src: 'src/**/images/*.*', dest: 'static/images' }
 				]
 			}),
-
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte', '.md'],
-				babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
-
 			!dev && terser({
 				module: true
 			})
