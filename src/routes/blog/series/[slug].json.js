2e18f1c8e3c238e3bd12015d3162255d68fc415f
Ryan Filler <ryanfiller89@gmail.com>
2021-05-03 08:14:15 -0500

    don't hard code port numbers for data fetching

--- src/routes/blog/series/[slug].json.js
+++ src/routes/blog/series/[slug].json.js
@@ -7,7 +7,7 @@ export async function get(req, res) {
 
   const slug = req.path.match(/(.*)\.json/)[1]
 
-	const series = await fetch('http://localhost:3000/blog/series.json')
+	const series = await fetch(`http://localhost:${process.env.PORT}/blog/series.json`)
     .then(response => response.json())
     .then(series => series.find(series => series.slug === slug))
 
