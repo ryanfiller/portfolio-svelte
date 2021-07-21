6c979f94cd333819344754bf2b080c09f3982d01
Ryan Filler <ryanfiller89@gmail.com>
2021-07-21 17:28:03 -0500

    install and configure eslint and stylelint

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  plugins: [
+    'svelte3',
+    'cypress'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  ignorePatterns: [
+    '.netlify',
+    '.svelte-kit',
+    '.vscode',
+    'build',
+  ],
+  // rules: {
+  //   // ...
+  // },
+  // settings: {
+  //   // ...
+  // }
+}
