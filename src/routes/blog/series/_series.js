47e2aa72faada41c24fa75f44c39b16b40cb3eac
Ryan Filler <ryanfiller89@gmail.com>
2021-08-13 09:04:41 -0500

    writing, also change the url

---
+++ src/routes/blog/series/_series.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    title: 'Dark Mode',
+    excerpt: 'Exploring the Intricacies of Adding Dark and Light Colors Schemes'
+  },
+  {
+    title: 'Docker Home Server',
+    excerpt: '(Finally) Learning to use Docker to set up a Home Server, with emphasis on visual tools'
+  }
+]
