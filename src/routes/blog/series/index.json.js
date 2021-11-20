21591cc40653f2d18a65a9e54d75697c5c9a6071
ryan filler <ryanfiller89@gmail.com>
2021-11-20 09:35:11 -0600

    turns out you can't fetch from another endpoint always so don't do that

--- src/routes/blog/series/index.json.js
+++ src/routes/blog/series/index.json.js
@@ -1,15 +1,17 @@
-import { sortNewestToOldest } from '../../../helpers'
+import { buildPagesJson, sortNewestToOldest } from '../../../helpers'
 
 export async function get({ host }) {
 	// this is gonna be a weird one...
 	const series = import.meta.globEager('/src/routes/_content/blog/series/**/index.md')
 
-	const posts = await fetch(`http://${host}/blog.json`)
-		.then(response => response.json())
-		.then(posts => {
-			return posts.filter(post => !!post.series)
-		})
-		.catch(error => console.error(error))
+	// can't fetch here, so just get the data the long way
+	let posts = import.meta.globEager('/src/routes/_content/blog/**/index.md')
+	const excludedPaths = [
+		'blog/tips/',
+		'blog/series/'
+	]
+	posts = buildPagesJson(posts, excludedPaths)
+	posts = JSON.parse(posts)
 
 	const formattedSeries = Object.entries(series)
 		.map(([path, component]) => {
