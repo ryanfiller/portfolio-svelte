59ad85c13bf73f1c2ab4e0bad37eb84f7bc0e9df
ryanfiller <ryanfiller89@gmail.com>
2022-07-07 15:22:29 -0500

    install postcss-preset-env

--- src/plugins/postcss/index.js
+++ src/plugins/postcss/index.js
@@ -1,10 +1,17 @@
+import presetEnv from 'postcss-preset-env'
 import autoprefixer from 'autoprefixer'
-import nesting from 'postcss-nesting'
 import customMedia from 'postcss-custom-media'
 
 export default [
+  // https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
+  presetEnv({
+    stage: 3,
+    features: {
+      'nesting-rules': true,
+      'lab-function': true
+    }
+  }),
   autoprefixer,
-  nesting,
   customMedia({
     importFrom: [
       // this is relative to svelte.config.js
