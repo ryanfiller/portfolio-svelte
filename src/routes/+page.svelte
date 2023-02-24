a2db8554c780c1dfa7a06f981aadc3630d24abd5
ryanfiller <ryanfiller89@gmail.com>
2023-02-24 16:19:47 -0600

    add <Logo> component and rudimentary test, add  alias

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
+<script>
+	import Logo from '$components/logo.svelte';
+</script>
+
 <main>
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<Logo />
+	<!-- <h1>Welcome to SvelteKit</h1> -->
+	<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 </main>
 
-<style>
-	main {
+<style lang="postcss">
+	:global(body) {
+		width: 100dvw;
+		height: 100dvh;
+		overflow: hidden;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	/* main {
 		& h1 {
 			color: lime;
 
@@ -12,5 +26,5 @@
 				color: pink;
 			}
 		}
-	}
+	} */
 </style>
