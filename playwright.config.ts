99245728505e3783c4fdc3fbc6bfdbde79160456
ryanfiller <ryanfiller89@gmail.com>
2023-03-07 08:02:29 -0600

    testing config  - move test directories  - install playwrite axe  - add temporary bug fix to vite config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,8 @@ const config: PlaywrightTestConfig = {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testDir: 'tests/e2e'
-}
+	testDir: 'tests/e2e',
+	testMatch: /.*.test.(js|ts)/
+};
 
 export default config
