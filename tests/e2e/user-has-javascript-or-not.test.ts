3769db14c41175c58e665a41ebfba91b75e8faab
ryanfiller <ryanfiller89@gmail.com>
2023-05-12 17:24:39 -0500

    add no-js functionality and test

---
+++ tests/e2e/user-has-javascript-or-not.test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('user has javascript', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/');
+	});
+
+	test('it removes the no-js data attribute', async ({ page }) => {
+		const noJS = await page.getByTestId('body').getAttribute('data-no-js')
+		expect(noJS).toEqual(null);
+	})
+})
+
+test.describe('user has disabled javascript', () => {
+	test.use({ javaScriptEnabled: false })
+
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/');
+	});
+
+	test('it does not remove the no-js data attribute', async ({ page }) => {
+		const noJS = await page.getByTestId('body').getAttribute('data-no-js')
+		// the tag is there, it just has no value
+		expect(noJS).toEqual('');
+	})
+})
